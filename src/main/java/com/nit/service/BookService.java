c952a3b6fbe038f0f1fa325edad112837f969230
anni <anni@gmail.com>
2019-09-18 17:53:44 +0530

    Signed-off-by: anni <anni@gmail.com>

--- src/main/java/com/nit/service/BookService.java
+++ src/main/java/com/nit/service/BookService.java
@@ -4,4 +4,7 @@ public class BookService {
 	public void addBook() {
 		System.out.println("Book added successfully");
 	}
+	public void deleteBook() {
+		System.out.println("Book is deleted form db...");
+	}
 }
